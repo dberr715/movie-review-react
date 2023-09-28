889dba649f27c1f681eea599f119ac1c605412e5
David Berry <dberry3755@gmail.com>
2023-09-28 13:11:59 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { MovieFetch } from "./components/MovieFetch";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MovieFetch />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MovieFetch.jsx
@@ -0,0 +1,3 @@
+export const MovieFetch = () => {
+  return <p>Hola</p>;
+};
